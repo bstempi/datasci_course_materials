35ecee1ec1e7627f8e20e7964cf69ba7a2bbcf4b
Brian Stempin <brian.stempin@gmail.com>
2014-07-06 15:29:46 -0700

    Implementing tweet scoring

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,40 @@
+import json
 import sys
 
-def hw():
-    print 'Hello, world!'
+def generate_sent_map(sent_lines):
+    my_dict = dict()
+    for line in sent_lines:
+        line_elements = line.split('\t')
+        my_dict[line_elements[0]] = int(line_elements[1].strip())
+    return my_dict
 
 def lines(fp):
-    print str(len(fp.readlines()))
+    return fp.readlines()
+
+def score_tweet(tweet_text, sent_map):
+    total_score = 0
+    words = tweet_text.split(' ')
+    for word in words:
+        word = word.strip()
+        score = sent_map.get(word, None)
+        if score:
+            total_score += score
+    return total_score
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_lines = lines(sent_file)
+    tweet_lines = lines(tweet_file)
+    sent_map = generate_sent_map(sent_lines)
+
+    for tweet_line in tweet_lines:
+        tweet = json.loads(tweet_line)
+        tweet_text = tweet.get('text', None)
+        if not tweet_text:
+            print 0
+        else:
+            print score_tweet(tweet_text, sent_map)
 
 if __name__ == '__main__':
     main()
