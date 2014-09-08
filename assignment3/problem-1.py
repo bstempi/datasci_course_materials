40a494cd25bc01a094fff2739a27508d44b3e569
Brian Stempin <brian.stempin@gmail.com>
2014-09-08 16:39:51 -0700

    First stab at problem 1

---
+++ assignment3/problem-1.py
@@ -0,0 +1,33 @@
+import MapReduce
+import sys
+from sets import Set
+
+"""
+Problem 1 -- building the inverted index
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of documents the word appears in
+    value_set = Set(list_of_values)
+    mr.emit((key, list(value_set)))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
